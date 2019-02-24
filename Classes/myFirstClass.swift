a2270f8c070a15d5de2340cd824c2708fe78d6d3
AjaY… <ajay....@AjaYs-MacBook-Air.local>
2019-02-25 00:12:56 +0530

    new123

--- Classes/myFirstClass.swift
+++ Classes/myFirstClass.swift
@@ -8,27 +8,134 @@
 import Foundation
 public class sevice {
     private init(){}
-    public static func stringNew() -> String{
-        return "ajay"
-    }
-    public static func stringNewygh() -> String{
-        return "ajay"
+    public func hexStringToUIColor (hex:String) -> UIColor
+    {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
     
-    public static func stringNewyghhbjn() -> String{
-        return "ajay"
+    public func hexStringToUIColorWaiting (hex:String) -> UIColor
+    {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(0.1)
+        )
     }
     
-    public static func stringNewyghhbyhjjn() -> String{
-        return "ajay"
+    public func validateEmail(enteredEmail:String) -> Bool
+    {
+        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: enteredEmail)
+    }
+   
+    
+    public func getDirectoryPath() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
     }
     
-    public static func stringNewcccyghhbjn() -> String{
-        return "ajay"
+    
+    public func colorWith(red : CGFloat,green : CGFloat, blue : CGFloat,alpha : CGFloat) -> UIColor
+    {
+        return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
     }
     
-    public static func stringNewccyghhbyhjjn() -> String{
-        return "ajay"
+    public func image(fromLayer layer: CALayer) -> UIImage {
+        
+        UIGraphicsBeginImageContext(layer.frame.size)
+        layer.render(in: UIGraphicsGetCurrentContext()!)
+        let outputImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return outputImage!
     }
+    
+    public func calcAge(birthday: String) -> Int {
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "yyyy/MM/dd"
+        let birthdayDate = dateFormater.date(from: birthday)
+        let calendar: NSCalendar! = NSCalendar(calendarIdentifier: .gregorian)
+        let now = Date()
+        let calcAge = calendar.components(.year, from: birthdayDate!, to: now, options: [])
+        let age = calcAge.year
+        return age!
+    }
+    
+    public func heightForView(text:String, font:UIFont, width:CGFloat) -> CGFloat
+    {
+        let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: CGFloat.greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byCharWrapping
+        label.font = font
+        
+        let attrString = NSMutableAttributedString(string: text)
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 2 // change line spacing between paragraph like 36 or 48
+        attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value: style, range: NSMakeRange(0, attrString.length))
+        label.attributedText = attrString
+        // label.text = text
+        label.sizeToFit()
+        return label.frame.height
+    }
+  
+    public func json(from object:Any) -> String? {
+        guard let data = try? JSONSerialization.data(withJSONObject: object, options: []) else {
+            return nil
+        }
+        return String(data: data, encoding: String.Encoding.utf8)
+    }
+    
+    public func gradient(from c1: UIColor?, to c2: UIColor?, withHeight height: Int) -> UIColor? {
+        let size = CGSize(width: 1, height: CGFloat(height))
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        let context = UIGraphicsGetCurrentContext()
+        let colorspace = CGColorSpaceCreateDeviceRGB()
+        
+        let colors = [c1?.cgColor, c2?.cgColor]
+        let gradient = CGGradient(colorsSpace: colorspace, colors: colors as? CFArray? as! CFArray, locations: nil)
+        context!.drawLinearGradient(gradient!, start: CGPoint(x: 0, y: 0), end: CGPoint(x: 0, y: size.height), options: [])
+        
+        let image: UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        if let anImage = image {
+            return UIColor(patternImage: anImage)
+        }
+        return nil
+    }
+    
     
 }
--- ajayframework.podspec
+++ ajayframework.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'ajayframework'
-  s.version          = '0.1.2'
+  s.version          = '0.1.3'
   s.summary          = 'A demo framework by ajay.'
 
   s.description      = <<-DESC
