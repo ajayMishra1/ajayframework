1403f2b5c4c9653093be47efe76823348b742259
AjaY… <ajay....@AjaYs-MacBook-Air.local>
2019-02-24 22:01:08 +0530

    ajay11

--- Classes/myNewFile.swift
+++
@@ -1,8 +0,0 @@
-//
-//  myNewFile.swift
-//  ajayframework
-//
-//  Created by AjaY….. on 24/02/19.
-//
-
-import Foundation
---
+++ myNewFile.swift
@@ -0,0 +1,15 @@
+//
+//  myNewFile.swift
+//  ajayframework
+//
+//  Created by AjaY….. on 24/02/19.
+//
+
+import Foundation
+
+public class sevice {
+    private init(){}
+    public static func stringNew() -> String{
+        return "ajay"
+    }
+}
