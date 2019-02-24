2347a5334fcc273d4778b67770f9fa4101efc5f6
AjaY… <ajay....@AjaYs-MacBook-Air.local>
2019-02-24 22:30:40 +0530

    new1333

---
+++ ajayframework/Classes/sffff.swift
@@ -0,0 +1,14 @@
+//
+//  sffff.swift
+//  ajayframework
+//
+//  Created by AjaY….. on 24/02/19.
+//
+
+import Foundation
+public class sevice {
+    private init(){}
+    public static func stringNew() -> String{
+        return "ajay"
+    }
+}
